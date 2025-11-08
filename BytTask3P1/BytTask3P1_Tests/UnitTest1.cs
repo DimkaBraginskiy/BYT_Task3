830543160f5ff481cb37c620eeeb98f24d84f1ee
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-11-08 15:53:42 +0100

    feat: Task1 initial commit

---
+++ BytTask3P1/BytTask3P1_Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BytTask3P1_Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
